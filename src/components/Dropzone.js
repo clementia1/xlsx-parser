74af5d00233c15825be9e994ab950f675cba48e0
clementia1 <grininma0@gmail.com>
2019-09-03 15:35:00 +0300

    *

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -65,7 +65,7 @@ function onDrop(acceptedFiles) {
    req.query('removeEmptyPhones=' + removeEmptyPhones);
 	req.end(function(err, res){
 		console.log(res.text);
-		if (res.text == 'job done') setReadyXls(true);		
+		setReadyXls(true);
 	});
 }
 
