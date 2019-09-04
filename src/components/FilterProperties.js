17a32d401c5e59360d29ea27f1a51f8f25bf5c16
clementia1 <grininma0@gmail.com>
2019-09-04 11:35:22 +0300

    *

--- src/components/FilterProperties.js
+++ src/components/FilterProperties.js
@@ -4,10 +4,15 @@ import columns from './Images/Columns.png';
 
 function FilterProperties() {
   return (
-	<div className="image-block">
-		<img className="eias-image" src={filter} alt="Filter" />
-		<img className="eias-image" src={columns} alt="Columns" />
-	</div>
+	<section className="container small-padding">
+		<div className="description">
+			<h5>Необхідно дотримуватись параметрів фільтру</h5>
+		</div>
+		<div className="image-block">		
+			<img className="eias-image" src={filter} alt="Filter" />
+			<img className="eias-image" src={columns} alt="Columns" />
+		</div>
+	</section>
 	)
 }
 
